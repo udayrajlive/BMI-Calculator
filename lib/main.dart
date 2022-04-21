2609580f7f25621d2a193df513314c21d76a1ab1
udayraj <udayr7396@gmail.com>
2022-04-22 01:21:03 +0530

    BMI calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -13,68 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'BMI',
       theme: ThemeData(
-          primaryColor: Colors.black,
-
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-
-        child: Column(
-
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline4,
-            ),
-          ],
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.purple,
         ),
+          scaffoldBackgroundColor: Colors.blueAccent,
+          textTheme: const TextTheme( bodyText2: TextStyle(color: Colors.white) )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-
-      ),
+      home: const MyHomePage(title: 'BMI Calculator '),
     );
   }
 }
+
+
